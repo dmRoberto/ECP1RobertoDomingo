35128e7b4ce109960635e6c4e697dc0b9d43a43b
dmRoberto <roberto.d20@gmail.com>
2014-10-08 20:04:27 +0200

    Roberto Domingo Martinez

---
+++ src/main/java/ECP1/Roberto/Domingo/App.java
@@ -0,0 +1,13 @@
+package ECP1.Roberto.Domingo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
